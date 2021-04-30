862dc8d9faab132c604842bf3eb5f3307e89978c
hwillians <66992569+hwillians@users.noreply.github.com>
2021-04-30 14:56:13 +0200

    class employé -> public

--- DesignPatternApp/Employe.cs
+++ DesignPatternApp/Employe.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DesignPatternApp
 {
-    class Employe
+    public class Employe
     {
         public int Id { get; set; }
         public string Nom { get; set; }
