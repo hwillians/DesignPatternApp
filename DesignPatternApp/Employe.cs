91c9147d46765d05da6dfe35c81d604b1f49722e
hwillians <66992569+hwillians@users.noreply.github.com>
2021-04-30 13:36:38 +0200

    creation classe employé

---
+++ DesignPatternApp/Employe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternApp
+{
+    class Employe
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+        private double salaire;
+
+        public double Salaire
+        {
+            get { return salaire; }
+            set
+            {
+                if (value > 0) salaire = value;
+                else throw new Exception("Le salaire doit être supérieur à 0");
+            }
+        }
+    }
+}
