406376331e5353ece7fafbf77cf009417cb64f10
hwillians <66992569+hwillians@users.noreply.github.com>
2021-04-30 14:56:28 +0200

    creation classe LocalStorage

---
+++ DesignPatternApp/LocalStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternApp
+{
+    public sealed class LocalStorage
+    {
+        private static LocalStorage instance = null;
+
+        public List<Employe> Employes = new() { };
+
+        private LocalStorage() { }
+
+        public static LocalStorage Instance
+        {
+            get
+            {
+                if (instance == null) instance = new LocalStorage();
+                return instance;
+            }
+        }
+    }
+}
